b2743e4d2bfc5e813e7aa60061a83b914f1f74b7
soumenganguly <soumendotganguly@gmail.com>
2015-05-01 16:07:36 +0530

    Modified the setup.py script

--- setup.py
+++ setup.py
@@ -1,63 +1,14 @@
 #! /usr/bin/python
 
 import subprocess
-
-
-#Installing Virtualenv
-wget_call = subprocess.call("wget https://pypi.python.org/packages/source/v/virtualenv/virtualenv-12.1.1.tar.gz", shell=True)
-if wget_call==0:
-    tar_call = subprocess.call("tar xf virtualenv-12.1.1.tar.gz", shell=True)
-    if tar_call==0:
-        setup_call = subprocess.call("sudo python virtualenv-12.1.1/setup.py install", shell=True)
-        if setup_call==0:
-                print "Successfully installed virtualenv"
-                #create and activate the virtualenv
-                create_venv = subprocess.call(["virtualenv","online_test"])
-                if create_venv==0:
-                    print "Virtualenv created"
-                    #Install the dependencies inside the virtualenv"
-                    install_dep = subprocess.call("source online_test/bin/activate && easy_install git+https://github.com/FOSSEE/online_test.git#egg=django_exam-0.1", shell=True)
-                    if install_dep==0:
-                        print "Dependencies installed"
-                        #Create a django project
-                        create_project = subprocess.call("source online_test/bin/activate && django-admin.py startproject django_online_test", shell=True)
-                        if create_project==0:
-                            print "Online test project created"
-                            #Manipulate the project settings
-                            subprocess.call("touch django_online_test/django_online_test/new_settings.py", shell=True)
-                            manipulate_settings()
-                            subprocess.call("rm settings.py", shell=True)
-                            new_settings = subprocess.call("mv new_settings.py settings.py", shell=True)
-                            subprocess.call("touch django_online_test/django_online_test/new_urls.py", shell=True)
-                            manipulate_urls()
-                            subprocess.call("rm urls.py", shell=True)
-                            new_urls = subprocess.call("mv new_urls.py urls.py", shell=True)
-                            if(new_settings==0 and new_urls==0):
-                                #Running the application on localhost
-                                run_app = subprocess.call("source online_test/bin/activate && python django_online_test/manage.py runserver")
-                                if run_app==0:
-                                    print "The online test interface is running, please point your browser to http://localhost:8000/exam"
-                                else:
-                                    print "online test app failed to load"
-                            else:
-                                print "The project setting's could not be altered"
-                        else:
-                            print "online test project creation failed"
-                    else:
-                        print "Dependencies could not be installed"
-                else:
-                    print "Virtualenv creation failed"
-        else:
-            print "Could not install virtualenv"
-    else:
-        print "tar call failed"
-else:
-    print "wget call failed"
-
+import os
+import sys
+pwd = os.getcwd()
+sys.path.append(pwd+'/django_online_test/django_online_test')
 
 def manipulate_settings():
     import settings
-    new_file = open("new_settings.py","a")
+    new_file = open("django_online_test/django_online_test/new_settings.py","a")
 
     new_file.write("import os")
     new_file.write("\n\n")
@@ -120,7 +71,7 @@ def manipulate_settings():
 
 
 def manipulate_urls():
-    new_urls_file = open("new_urls.py","a")
+    new_urls_file = open("django_online_test/django_online_test/new_urls.py","a")
 
     new_urls_file.write("from django.conf.urls import pattern, include, url")
     new_urls_file.write("\n\n")
@@ -135,4 +86,55 @@ def manipulate_urls():
 
     new_urls_file.close()
     
-    
+
+#Installing Virtualenv
+wget_call = subprocess.call("wget https://pypi.python.org/packages/source/v/virtualenv/virtualenv-12.1.1.tar.gz", shell=True)
+if(wget_call==0):
+    tar_call = subprocess.call("tar xf virtualenv-12.1.1.tar.gz", shell=True)
+    if(tar_call==0):
+        setup_call = subprocess.call("cd virtualenv-12.1.1 && sudo python setup.py install", shell=True)
+        if(setup_call==0): #create and activate the virtualenv
+                print "Successfully installed virtualenv"
+                create_venv = subprocess.call(["virtualenv","online_test"])
+                if(create_venv==0): #Install the dependencies inside the virtualenv"
+                    print "Virtualenv created"
+                    install_dep = subprocess.call("online_test/bin/easy_install git+https://github.com/FOSSEE/online_test.git#egg=django_exam-0.1", shell=True)
+                    if(install_dep==0): #Create a django project
+                        print "Dependencies installed"
+                        create_project = subprocess.call("online_test/bin/django-admin.py startproject django_online_test", shell=True)
+                        if(create_project==0):
+                            print "Online test project created"
+                            
+                            #Manipulate the project settings
+                            subprocess.call("touch django_online_test/django_online_test/new_settings.py", shell=True)
+                            manipulate_settings()
+                            subprocess.call("rm django_online_test/django_online_test/settings.py", shell=True)
+                            new_settings = subprocess.call("mv django_online_test/django_online_test/new_settings.py django_online_test/django_online_test/settings.py", shell=True)
+                            
+                            #Manipulate URL's
+                            subprocess.call("touch django_online_test/django_online_test/new_urls.py", shell=True)
+                            manipulate_urls()
+                            subprocess.call("rm django_online_test/django_online_test/urls.py", shell=True)
+                            new_urls = subprocess.call("mv django_online_test/django_online_test/new_urls.py django_online_test/django_online_test/urls.py", shell=True)
+                            if(new_settings==0 and new_urls==0): #Running the application on localhost
+                                sync_db = subprocess.call("online_test/bin/python django_online_test/manage.py syncdb", shell=True)
+                                run_app = subprocess.call("online_test/bin/python django_online_test/manage.py runserver", shell=True)
+                                if(sync_db and run_app==0):
+                                    print "The online test interface is running, please point your browser to http://localhost:8000/exam"
+                                else:
+                                    print "online test app failed to load"
+                            else:
+                                print "The project setting's could not be altered"
+                        else:
+                            print "online test project creation failed"
+                    else:
+                        print "Dependencies could not be installed"
+                else:
+                    print "Virtualenv creation failed"
+        else:
+            print "Could not install virtualenv"
+    else:
+        print "tar call failed"
+else:
+    print "wget call failed"
+
